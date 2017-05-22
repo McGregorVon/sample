cef94b068a5d359edd3ba55cab2a808589369ce4
McGregorVon <844470060@qq.com>
2017-05-22 14:46:42 +0000

    finish layouts and route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">HahaLi fix conflict - 李菁华</div>
             </div>
         </div>
     </body>
