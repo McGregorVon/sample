2c04e90f23614cc86d2185153e647c736c0cbfa5
McGregorVon <844470060@qq.com>
2017-05-22 13:57:15 +0000

    finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','sample')
+@section('content')
+  <h1>主页</h1>
+@stop
