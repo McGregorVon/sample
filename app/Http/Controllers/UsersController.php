91f5613e98db7f7992abc5d8c7cf431fdc28e6be
McGregorVon <844470060@qq.com>
2017-05-24 14:10:54 +0000

    finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Controller;
 
 use App\Models\User;
 
+use Auth;
+
 class UsersController extends Controller
 {
     public function create()
@@ -35,7 +37,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,12 +10,17 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+//静态页面路由
 get('/laravel', function () {
     return view('welcome');
 });
 get('/', 'StaticPagesController@home')->name('home');
 get('/help', 'StaticPagesController@help')->name('help');
 get('/about', 'StaticPagesController@about')->name('about');
+//用户路由
 get('signup','UsersController@create')->name('signup');
 resource('users','UsersController');
+//会话路由
+get('login','SessionsController@create')->name('login');
+post('login','SessionsController@store')->name('store');
+delete('logout','SessionsController@destroy')->name('logout');
