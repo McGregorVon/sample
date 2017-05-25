bfa237615dca4b144a9b3085c8eb71c86bdf4e60
McGregorVon <844470060@qq.com>
2017-05-25 05:57:40 +0000

    finish user curd

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,6 +11,14 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
     public function create()
     {
         return view('sessions.create');
@@ -29,12 +37,12 @@ class SessionsController extends Controller
        ];
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
-          session()->flash('success', '欢迎回来！');
-          return redirect()->route('users.show', [Auth::user()]);
-      } else {
-          session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-          return redirect()->back();
-      }
+           session()->flash('success', '欢迎回来！');
+           return redirect()->intended(route('users.show', [Auth::user()]));
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
     }
     public function destroy()
     {
